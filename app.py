0270237310bb77ab392a28c1fe78a2c53f223eb4
Venkatesh Narayanan <venknar@users.noreply.github.com>
2019-10-13 10:50:13 +0530

    Image Classifier V1

--- app.py
+++ app.py
@@ -96,9 +96,12 @@ def upload():
         return result
     return None
 
-if __name__ == '__main__':
+
+app.run(debug=False, threaded=False)
+
+#if __name__ == '__main__':
     
-    app.run(debug=False, threaded=False)
+ #   app.run(debug=False, threaded=False)
 
     # Serve the app with gevent
     # http_server = WSGIServer(('0.0.0.0', 5000), app)
